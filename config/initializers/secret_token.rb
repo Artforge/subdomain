d8b33e0b5e33ed33436f88ea83f690d03b51acad
Adam Singer <adam@artforge.com>
2013-06-05 11:09:10 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subdomain::Application.config.secret_token = 'f82e069b051a57a8da4123bdec4a18ff50b19a032b7ea8254fdf02aa73ab4d6a7431f8762a11f0e9b09c0a2e5a85f2ed4288a565ca74f25734c39e32a40539c9'
